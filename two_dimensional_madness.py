db98334f250111ca0fa3a21d6c09fc2f4ebb531f
SergZhig <zse1@bk.ru>
2020-04-19 22:39:47 +0300

    digit2

--- two_dimensional_madness.py
+++ two_dimensional_madness.py
@@ -1,5 +1,6 @@
-from  colorama import init
-from colorama import Fore,Back, Style
+from colorama import init
+from colorama import Fore, Back, Style
+
 # use colorama to make Termcolor worc on Windows too
 init()
 
@@ -45,7 +46,7 @@ TWO = [
 
 class TwoDimensionalMadness:
 
-    def __init__(self, width, height, background=" ", mark="*", space="",color="black"):
+    def __init__(self, width, height, background=" ", mark="*", space="", color="black"):
         self.width = width
         self.height = height
         self.background = background
@@ -56,13 +57,12 @@ class TwoDimensionalMadness:
             '0': ZERO,
             '1': ONE,
             '2': TWO,
-            }
-        self.color=color
-
+        }
+        self.color = color
 
     def generate_plane(self):
         # Берет из self размеры и генерирует и возвращает plane заданного размера
-        #plane = []
+        # plane = []
         plane = [[self.background] * self.width for i in range(self.height)]
         return plane
 
@@ -72,16 +72,16 @@ class TwoDimensionalMadness:
         if plane is None:
             plane = self.plane
         for line in plane:
-            i=0
-            while i<len(line):
-                if line[i]==self.mark:
-                    line[i]=self.background
+            i = 0
+            while i < len(line):
+                if line[i] == self.mark:
+                    line[i] = self.background
                 else:
-                    line[i]=self.mark
-                i+=1
-        reversed_plane=plane
+                    line[i] = self.mark
+                i += 1
+        reversed_plane = plane
         if reflection:
-             reversed_plane = plane[::-1]
+            reversed_plane = plane[::-1]
         return reversed_plane
 
     def print_plane(self, reversed=False, reflect=False):
@@ -90,11 +90,10 @@ class TwoDimensionalMadness:
         else:
             self.print_something(self.plane)
 
-
     def set_color(self):
-        if self.color=="black":
+        if self.color == "black":
             print(Fore.BLACK)
-        elif self.color=="green":
+        elif self.color == "green":
             print(Fore.GREEN)
         elif self.color == "blue":
             print(Fore.CYAN)
@@ -113,7 +112,7 @@ class TwoDimensionalMadness:
 
     def string_to_plane(self, some_string):
         # врендеривает строку в плейн.
-        str_line=[]
+        str_line = []
 
         for i in range(len(ZERO)):
             a = []
@@ -133,14 +132,12 @@ class TwoDimensionalMadness:
 
         for j in range(iter_j):
             for i in range(iter_i):
-                if str_line[j][i]==self.mark or self.plane[j][i] ==self.mark:
+                if str_line[j][i] !=" " or self.plane[j][i] == self.mark:
                     self.plane[j][i] = self.mark
         return self.plane
 
 
-
-
-plane_object = TwoDimensionalMadness(height=15, width=20, background=" ", mark="*", space=" ",color='blue')
+plane_object = TwoDimensionalMadness(height=15, width=20, background=" ", mark="A", space=" ", color='blue')
 
 plane_object.put_mark(height=5, width=10)
 plane_object.put_mark(height=5, width=8)
@@ -154,5 +151,4 @@ plane_object.put_mark(height=10, width=9)
 
 plane_object.string_to_plane(some_string='101')
 
-plane_object.print_plane(reversed=False,reflect=False)
-
+plane_object.print_plane(reversed=False, reflect=False)
